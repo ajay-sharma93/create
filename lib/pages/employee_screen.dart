1cc5d22ace42b257620f84769b3659936a733b40
ajaysharma <sharma.ajaya2019@gmail.com>
2025-07-22 22:54:47 +0545

    employee form added

--- lib/pages/employee_screen.dart
+++ lib/pages/employee_screen.dart
@@ -1,35 +1,35 @@
-import 'package:crud/pages/employee_screen.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class EmployeeScreen extends StatefulWidget {
+  const EmployeeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<EmployeeScreen> createState() => _EmployeeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _EmployeeScreenState extends State<EmployeeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        Navigator.push(context, MaterialPageRoute(builder: (context)=>EmployeeScreen(),));
-      },child: Icon(Icons.add)),
-      appBar: AppBar(
+       appBar: AppBar(
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
         Text("Flutter",  
           style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24),),
           SizedBox(width: 5,),
-        Text("Firebase",  
+        Text("Form",  
           style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue,fontSize: 24),),
 
           ],
         ),
       ),
       body: Container(
-        child: Column(children: [],),
+        child: Column(
+          children: [
+            Text('Employee Screen'),
+          ],
+        ),
       ),
     );
   }
